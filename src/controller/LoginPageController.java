6f9ed23e561dd3ff5930e093b00cd3055fe68a95
Mohammadreza <mryamahdi@yahoo.com>
2020-07-15 17:48:02 +0430

    login page and sign up page are added

---
+++ src/controller/LoginPageController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+
+public class LoginPageController {
+
+    @FXML
+    TextField user;
+    @FXML
+    TextField pass;
+
+    @FXML
+    Label wrongLabel;
+
+    public void loginPressed(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/controller/SignUpPageController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SignUpPageController {
+    @FXML
+    TextField user;
+    @FXML
+    TextField pass;
+
+    public void createPressed(ActionEvent actionEvent) {
+    }
+}
